ef7b51dd66354099608c84caf92c7dba9695dbc8
ian <i@ianisme.com>
2016-03-11 09:30:17 +0800

    init

---
+++ IANXMPP_DEMO/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  IANXMPP_DEMO
+//
+//  Created by ian on 16/3/10.
+//  Copyright © 2016年 ian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
